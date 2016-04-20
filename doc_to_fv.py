6c70476ec48f685a53d7a939217b458d4cb4425b
shirasaki-13046 <sd13046@toyota-ti.ac.jp>
2016-04-20 14:46:45 +0900

    doc_to_fvの修正

--- doc_to_fv.py
+++ doc_to_fv.py
@@ -4,19 +4,38 @@ from nltk.tokenize import sent_tokenize,word_tokenize
 
 file=open(sys.argv[1]).read()
 lines=sent_tokenize(file)
-dic=shelve.open("shelve_file")
-dic1={}
+dic1=shelve.open("shelve_file2")
 dic2={}
-i=0
+
 for line in lines:
     words=word_tokenize(line)
     for word in words:
-        if word in dic1:
-            dic1[word]+=1
-            dic2[dic[word]]=dic1[word]
+        if word not in dic1:
+            dic1[word]=len(dic1)+1
+
+for line in lines:
+    words=word_tokenize(line)
+    for word in words:
+        if dic1[word] in dic2:
+            dic2[dic1[word]]+=1
         else:
-            dic1[word]=1
-            dic2[dic[word]]=dic1[word]
-print(dic2)
+            dic2[dic1[word]]=1
+
+for res in dic2:
+    print(res,":",dic2[res]," ",end="")
+ 
+#dic1={}
+#dic2={}
+#i=0
+#for line in lines:
+#    words=word_tokenize(line)
+#    for word in words:
+#        if word in dic1:
+#            dic1[word]+=1
+#            dic2[dic[word]]=dic1[word]
+#        else:
+#            dic1[word]=1
+#            dic2[dic[word]]=dic1[word]
+#print(dic2)
 
 

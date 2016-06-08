73f4d682d0f5cda5492435e4a83d5c5cb305d99e
shirasaki-13046 <sd13046@toyota-ti.ac.jp>
2016-06-08 16:07:50 +0900

    2_9_8まで

--- perceptron.py
+++ perceptron.py
@@ -1,107 +1,119 @@
 import sys
-file=open(sys.argv[1]).readlines()
+import random
+import math
+from optparse import OptionParser
+train_file=open(sys.argv[1]).readlines()
 test_file=open(sys.argv[2]).readlines()
 
 #i番目のレビューのタプルを作る
-def read_instance(i):
+def read_instance(filename,i):
     vector_list=[]
-    review=file[i]
+    norm_bef=0
+    review=filename[i]
     label_sp=review.split("  ")
-    label=label_sp[0]
+    label=int(label_sp[0])
     vectors=label_sp[1].rstrip().split(" ")
     for vector in vectors:
         youso=vector.split(":")
-        vector_tup=(youso[0],youso[1])
+        hindo=int(youso[1])
+        norm_bef += hindo * hindo
+    norm_bunbo=math.sqrt(norm_bef)
+    for vector in vectors:
+        youso=vector.split(":")
+        index=int(youso[0])
+        hindo=int(youso[1])
+        if sys.argv[5] == str(1):
+            norm_bunbo=1
+        norm = hindo/norm_bunbo
+        vector_tup=(index,norm)
         vector_list.append(vector_tup)
+    bius=(int(0),int(sys.argv[4]))
+    vector_list.append(bius)
     res=(label,vector_list)
     vector_list=[]
     return res
 
 #i番目のレビューをリストに入れ、最大インデックスを抽出
-def read_data():
-    max_index=1
+def read_data(train_file,test_file):
+    max_index=0
     train_data=[]
-    for i in range(len(file)):
-        review=read_instance(i)
+    for i in range(len(train_file)):
+        review=read_instance(train_file,i)
         sosei=review[1]
         sosei_max=max(sosei,key=(lambda x: int(x[0])))
         review_max = int(sosei_max[0])
-        if int(review_max) > int(max_index):
+        if review_max > max_index:
             max_index=review_max
         train_data.append(review)
         i=i+1
-
-
-    test_list=[]
     test_data=[]
-    for j in range(len(test_file)):
-        review=test_file[j]
-        label_sp=review.split("  ")
-        label=label_sp[0]
-        vectors=label_sp[1].rstrip().split(" ")
-        for vector in vectors:
-            youso=vector.split(":")
-            vector_tup=(youso[0],youso[1])
-            test_list.append(vector_tup)
-        res=(label,test_list)        
-        test_data.append(res)
-        test_list=[]
+    for i in range(len(test_file)):
+        review=read_instance(test_file,i)
+        sosei=review[1]
+        test_data.append(review)
+        i=i+1
     return (train_data,max_index,test_data)
 
-
 #素性ベクトルを足す
-def add_fv(i):
-    for k in range(len(train_data[i][1])):
-        sosei=train_data[i][1][k]
-        index=int(sosei[0])
-        hindo=int(sosei[1])
-        weight_vector[index]=weight_vector[index]+hindo
+def add_fv(i,train):
+    for k in range(len(train)):
+        index=train[k][0]
+        hindo=train[k][1]
+        weight[index]+=hindo
+        upmath=hindo * nupdates[0]
+        tmp_weight[index]+=upmath
 
 #素性ベクトルを引く
-def sub_fv(i):
-    for k in range(len(train_data[i][1])):
-        sosei=train_data[i][1][k]
-        index=int(sosei[0])
-        hindo=int(sosei[1])
-        weight_vector[index]=weight_vector[index]-hindo
+def sub_fv(i,train):
+    for k in range(len(train)):
+        index=train[k][0]
+        hindo=train[k][1]
+        upmath=hindo * nupdates[0]
+        tmp_weight[index]-=upmath
+        weight[index]-=hindo
 
 #内積の計算
-def mult_fv(i):
+def mult_fv(i,filename,weight_vector):
     naiseki=0
-    for j in range(len(train_data[i][1])):
-        sosei=train_data[i][1][j]
-        index=int(sosei[0])
-        hindo=int(sosei[1])
+    for j in range(len(filename)):
+        index=filename[j][0]
+        hindo=filename[j][1]
         if index <= max_index:
-            naiseki_1=weight_vector[index]*hindo
-            naiseki+=naiseki_1
+            naiseki+=weight_vector[index] * hindo
     return naiseki
 
 #ベクトルの更新
 def update_weight():
+    random.seed(0)
+    random.shuffle(train_data)
     for i in range(len(train_data)):
-        naiseki_2=int(mult_fv(i))
-        label=int(train_data[i][0])
-        if naiseki_2 * label <= 0:
-            if label > 0:
-                add_fv(i)
+        train=train_data[i][1]
+        naiseki_2=mult_fv(i,train,weight)
+        label=train_data[i][0]        
+        if abs(naiseki_2) < float(sys.argv[6]) or naiseki_2*label<=0:
+            nupdates[0]+=1
+            if label > 0 :
+                add_fv(i,train)
             else:
-                sub_fv(i)
+                sub_fv(i,train)
+            for j in range(len(weight_vector_2)):
+                weight_vector_2[j]+=weight[j]
+
+#ベクトルの平均化
+def averaged_weight():
+    for i in range(len(weight)):
+        ave_weight[i]=weight[i] - (tmp_weight[i]/(nupdates[0]+1))
+    print(nupdates[0])
 
 #結果の計算
-def evaluate():
+def evaluate(weight_vector):
     naiseki=0
     seikai=0
     instance=0
     for i in range(len(test_data)):
-        label=int(test_data[i][0])
-        for j in range(len(test_data[i][1])):
-            sosei=test_data[i][1][j]
-            index=int(sosei[0])
-            hindo=int(sosei[1])
-            if index <= max_index:
-                naiseki_single=weight_vector[index]*hindo
-                naiseki+=naiseki_single
+        test=test_data[i][1]
+        label=test_data[i][0]
+        naiseki=mult_fv(i,test,weight_vector)
         if (naiseki * label) > 0:
             seikai+=1
         instance+=1
@@ -109,39 +121,36 @@ def evaluate():
     seikairitsu=(seikai/instance)*100
     return (seikai,instance,seikairitsu)
 
-weight=[]
-read_data=read_data()
-max_index=read_data[1]
-for dim in range(int(max_index)+1):
-    weight.append(0)
+read_data=read_data(train_file,test_file)
+max_index=int(read_data[1])
 
-weight_vector=weight
-train_data=read_data[0]
-test_data=read_data[2]
+if __name__=="__main__":
+    parser=OptionParser()
+    parser.add_option
+    weight=[0]*(max_index+1)
+    tmp_weight=[0]*(max_index+1)
+    ave_weight=[0]*(max_index+1)
+    weight_vector_2=[0]*(max_index+1)
+    train_data=read_data[0]
+    test_data=read_data[2]
+    nupdates=[0]*(max_index+1)
+    for r in range(int(sys.argv[3])):
+        update_weight()
+    averaged_weight()
+    for i in range(len(weight_vector_2)):
+        weight_vector_2[i]=weight_vector_2[i]/(nupdates[0]+1)
+    if sys.argv[7]==str(1):
+        print(evaluate(ave_weight))
+    else:
+        print(evaluate(weight))
 
-for r in range(5000):
-    update_weight()
-print(evaluate())
+#print(weight[0:5])
+#print(tmp_weight[0:50])
 
+#hikaku_list=[0]*(max_index+1)
+#for i in range(len(ave_weight)):
+#    hikaku_list[i]=ave_weight[i]-weight_vector_2[i]    
 
-
-#for r in range(1000):
-#    vector_result=update_weight()
-#    if r == 0:
-#        result=evaluate(vector_result)
-#        print("正解数 : ",result[0],"\n","インスタンス数 : ",result[1],"\n","正解率 : ",result[2],"%")
-#    if r == 50:
-#        result=evaluate(vector_result)
-#        print("正解数 : ",result[0],"\n","インスタンス数 : ",result[1],"\n","正解率 : ",result[2],"%")
-#    if r == 200:
-#        result=evaluate(vector_result)
-#        print("正解数 : ",result[0],"\n","インスタンス数 : ",result[1],"\n","正解率 : ",result[2],"%")
-#    if r == 400:
-#        result=evaluate(vector_result)
-#        print("正解数 : ",result[0],"\n","インスタンス数 : ",result[1],"\n","正解率 : ",result[2],"%")
-#    if r == 800:
-#        result=evaluate(vector_result)
-#        print("正解数 : ",result[0],"\n","インスタンス数 : ",result[1],"\n","正解率 : ",result[2],"%")
-#   if r == 1000:
-#        result=evaluate(vector_result)
-#        print("正解数 : ",result[0],"\n","インスタンス数 : ",result[1],"\n","正解率 : ",result[2],"%")
+#print(ave_weight[0:50])
+#print(weight_vector_2[0:50])
+#print(hikaku_list[0:50])
